1c9b7353fab0890285a4355ca9a722ca0856cda5
yinmi <yinmichun@qq.com>
2023-07-24 18:50:28 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,13 @@
+﻿
+using Sunny.UI;
+
+namespace opencvNode
+{
+    public partial class Form1 : UIForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
