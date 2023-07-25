577c94f23510437d28d0eea34b8036eec717f5cc
yinmi <yinmichun@qq.com>
2023-07-25 15:39:49 +0800

    基本界面

---
+++ Form3.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace opencvNode
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stNodeTreeView1 = new ST.Library.UI.NodeEditor.STNodeTreeView();
+            this.stNodeEditor1 = new ST.Library.UI.NodeEditor.STNodeEditor();
+            this.SuspendLayout();
+            // 
+            // stNodeTreeView1
+            // 
+            this.stNodeTreeView1.AllowDrop = true;
+            this.stNodeTreeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.stNodeTreeView1.FolderCountColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.stNodeTreeView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.stNodeTreeView1.ItemBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.stNodeTreeView1.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.stNodeTreeView1.Location = new System.Drawing.Point(0, -2);
+            this.stNodeTreeView1.MinimumSize = new System.Drawing.Size(100, 60);
+            this.stNodeTreeView1.Name = "stNodeTreeView1";
+            this.stNodeTreeView1.ShowFolderCount = true;
+            this.stNodeTreeView1.Size = new System.Drawing.Size(177, 672);
+            this.stNodeTreeView1.TabIndex = 0;
+            this.stNodeTreeView1.Text = "stNodeTreeView1";
+            this.stNodeTreeView1.TextBoxColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.stNodeTreeView1.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            // 
+            // stNodeEditor1
+            // 
+            this.stNodeEditor1.AllowDrop = true;
+            this.stNodeEditor1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.stNodeEditor1.Curvature = 0.3F;
+            this.stNodeEditor1.Location = new System.Drawing.Point(174, -2);
+            this.stNodeEditor1.LocationBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MarkBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MarkForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.stNodeEditor1.Name = "stNodeEditor1";
+            this.stNodeEditor1.Size = new System.Drawing.Size(1253, 672);
+            this.stNodeEditor1.TabIndex = 1;
+            this.stNodeEditor1.Text = "stNodeEditor1";
+            // 
+            // Form3
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1426, 668);
+            this.Controls.Add(this.stNodeEditor1);
+            this.Controls.Add(this.stNodeTreeView1);
+            this.Name = "Form3";
+            this.PageIndex = 1003;
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ST.Library.UI.NodeEditor.STNodeTreeView stNodeTreeView1;
+        private ST.Library.UI.NodeEditor.STNodeEditor stNodeEditor1;
+    }
+}
